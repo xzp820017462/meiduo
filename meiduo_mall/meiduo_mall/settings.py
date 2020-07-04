f95a9a68abd79ef55cd3c7344c5ed6e29defcd43
xzp <820017462@qq.com>
2020-07-05 02:48:43 +0800

    add static file

--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -154,6 +154,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
+
+
 # LOGGING = {
 #     'version':1,
 #     'disable_existing_loggers':False, #是禁用已经存在的日志器
